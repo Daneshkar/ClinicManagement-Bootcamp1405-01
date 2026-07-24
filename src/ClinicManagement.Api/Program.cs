61ea4978d6760ccbb774ed07099d8d6dc94e955d
Mopano <p.r.7.norouzi@gmail.com>
2026-07-24 16:04:11 +0300

    add Application service to programm and DI

--- src/ClinicManagement.Api/Program.cs
+++ src/ClinicManagement.Api/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddSwaggerGen();
 // builder.Services.AddApplication();
 // builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
