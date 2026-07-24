021e5e748328763e2b6a4dc3cd98f1156ad39058
Mopano <p.r.7.norouzi@gmail.com>
2026-07-24 15:36:52 +0300

    add DI extension and LocalDB connection setup

--- src/ClinicManagement.Api/Program.cs
+++ src/ClinicManagement.Api/Program.cs
@@ -1,4 +1,5 @@
 using ClinicManagement.Application;
+using ClinicManagement.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddSwaggerGen();
 // builder.Services.AddApplication();
 // builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 // 4. Configure Request Pipeline
