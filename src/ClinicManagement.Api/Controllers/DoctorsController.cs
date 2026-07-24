b339cb0176f0777e124e64f0da850595dd126f5b
Mopano <p.r.7.norouzi@gmail.com>
2026-07-24 16:04:51 +0300

    Implementation Of doctor controller sign-up

---
+++ src/ClinicManagement.Api/Controllers/DoctorsController.cs
@@ -0,0 +1,35 @@
+﻿using ClinicManagement.Application.DTOs.Doctors;
+using ClinicManagement.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicManagement.Api.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DoctorsController : ControllerBase
+    {
+        private readonly IDoctorService _doctorService;
+
+        public DoctorsController(IDoctorService doctorService)
+        {
+            _doctorService = doctorService;
+        }
+
+        [HttpPost("signup")]
+        [ProducesResponseType(typeof(DoctorSignupResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(DoctorSignupResponseDto), StatusCodes.Status409Conflict)]
+        public async Task<IActionResult> Signup([FromBody] DoctorSignupRequestDto request)
+        {
+            var result = await _doctorService.SignupAsync(request);
+
+            if (!result.IsSuccess)
+            {
+                return Conflict(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
