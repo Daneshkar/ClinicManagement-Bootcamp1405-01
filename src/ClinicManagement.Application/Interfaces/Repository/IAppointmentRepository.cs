3001666786fb1aae17eaed1521718a9d4f4f8313
Mopano <p.r.7.norouzi@gmail.com>
2026-08-01 09:32:23 +0300

    Implementation Of IAppointmentRepository

---
+++ src/ClinicManagement.Application/Interfaces/Repository/IAppointmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using ClinicManagement.Domain.Entities;
+
+namespace ClinicManagement.Application.Interfaces.Repository
+{
+    public interface IAppointmentRepository
+    {
+        public Task<List<DateTime>> GetBookedVisitDatesAsync(string doctorMedicalId, DateTime date);
+        public Task<bool> ExistsAsync(string doctorMedicalId, DateTime visitDate);
+        public Task AddAsync(Appointment appointment);
+    }
+}
