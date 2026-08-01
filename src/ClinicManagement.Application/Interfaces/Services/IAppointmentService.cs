baf046a52ede54d27f6274ff5200b0172a7d62df
Mopano <p.r.7.norouzi@gmail.com>
2026-08-01 09:38:05 +0300

    Implementation Of IAppointmentService

---
+++ src/ClinicManagement.Application/Interfaces/Services/IAppointmentService.cs
@@ -0,0 +1,10 @@
+﻿using ClinicManagement.Application.DTOs.Appointments;
+
+namespace ClinicManagement.Application.Interfaces.Services
+{
+    public interface IAppointmentService
+    {
+        public Task<DoctorAvailableSlotsResponseDto> GetAvailableSlotsAsync(string doctorMedicalId, DateTime date);
+        public Task<AppointmentCreateResponseDto> BookAppointmentAsync(AppointmentCreateRequestDto appointmentCreateRequestDto);
+    }
+}
