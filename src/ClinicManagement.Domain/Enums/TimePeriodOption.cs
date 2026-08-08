6225d2c4d0c1813cfc4f90d1d032b63e7570400a
hadisfrg <h.farajpour1996@gmail.com>
2026-08-08 13:12:02 -0700

    Implement  TimePeriodOption Enum Domain/Enums/TimePeriodOption.cs

---
+++ src/ClinicManagement.Domain/Enums/TimePeriodOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagement.Domain.Enums
+{
+   public enum TimePeriodOption
+    {
+        AllTime = 0,
+        LastDay = 1,
+        LastWeek = 2,
+        LastMonth = 3,
+        Custom = 4
+    }
+}
+
