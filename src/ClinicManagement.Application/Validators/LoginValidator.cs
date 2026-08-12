23eda794558428b4402bb464fa1adf0a90b1c021
elena <HP@LAPTOP-GORF2L20>
2026-08-12 19:39:23 +0330

    login validator

---
+++ src/ClinicManagement.Application/Validators/LoginValidator.cs
@@ -0,0 +1,32 @@
+﻿using ClinicManagement.Application.DTOs.Auth;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagement.Application.Validators
+{
+    class LoginValidator
+        : AbstractValidator<DoctorLoginRequest>
+    {
+        public  LoginValidator() {
+
+            RuleFor(x => x.MedicalId)
+                   .NotEmpty()
+                   .WithMessage("Medical ID is required.")
+                   .MaximumLength(20)
+                   .WithMessage("Medical ID cannot exceed 20 characters.");
+
+            RuleFor(x => x.Password)
+              .NotEmpty()
+              .WithMessage("Password is required.")
+              .MinimumLength(8)
+              .WithMessage("Password must be at least 8 characters.");
+
+
+        }
+
+
+
+    }
+}
