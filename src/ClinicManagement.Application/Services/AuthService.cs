b88497d6203d3915d7f486edab65df0c17ecd201
elena <HP@LAPTOP-GORF2L20>
2026-08-12 20:48:47 +0330

    IDoctorRepository

---
+++ src/ClinicManagement.Application/Services/AuthService.cs
@@ -0,0 +1,19 @@
+﻿using ClinicManagement.Application.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagement.Application.Services
+{
+    public class AuthService:IAuthService
+    {
+        private readonly IDoctorRepository _doctorRepository;
+        public AuthService(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+
+
+    }
+}
