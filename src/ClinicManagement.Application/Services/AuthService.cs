38ce9042c9883152afc2807620b71911339f8f66
elena <HP@LAPTOP-GORF2L20>
2026-08-12 20:52:07 +0330

    IPasswordHasher

--- src/ClinicManagement.Application/Services/AuthService.cs
+++ src/ClinicManagement.Application/Services/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿using ClinicManagement.Application.Interfaces.Repository;
+using ClinicManagement.Application.Interfaces.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,6 +14,17 @@ namespace ClinicManagement.Application.Services
             _doctorRepository = doctorRepository;
         }
 
+        private readonly IRefreshTokenRepository _refreshTokenRepository;
+        public AuthService(IRefreshTokenRepository refreshTokenRepository)
+        {
+            _refreshTokenRepository = refreshTokenRepository;
+        }
+
+        private readonly IPasswordHasher _passwordHasher;
+        public AuthService(IPasswordHasher passwordHasher)
+        {
+            _passwordHasher = passwordHasher;
+        }
 
 
     }
