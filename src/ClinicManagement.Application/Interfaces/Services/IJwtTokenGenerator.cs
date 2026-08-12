63abb2646e3d69ae226b40ceb0db4a94a21d25e4
elena <HP@LAPTOP-GORF2L20>
2026-08-12 20:38:53 +0330

    IJwtTokenGenerator

---
+++ src/ClinicManagement.Application/Interfaces/Services/IJwtTokenGenerator.cs
@@ -0,0 +1,16 @@
+﻿using ClinicManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagement.Application.Interfaces.Services
+{
+    public interface IJwtTokenGenerator
+    {
+
+        public string GenerateAccessToken(Doctor  doctor);
+        public string GenerateRefreshToken();
+
+
+    }
+}
