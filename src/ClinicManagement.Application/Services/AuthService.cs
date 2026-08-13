f9166a20d288e81000c2dc14af67a5e6b0ef667e
elena <HP@LAPTOP-GORF2L20>
2026-08-13 07:37:42 +0330

    LoginAsync

--- src/ClinicManagement.Application/Services/AuthService.cs
+++ src/ClinicManagement.Application/Services/AuthService.cs
@@ -1,14 +1,17 @@
-﻿using ClinicManagement.Application.DTOs.Auth;
+﻿using ClinicManagement.Application.Common;
+using ClinicManagement.Application.DTOs.Auth;
 using ClinicManagement.Application.Interfaces.Repository;
 using ClinicManagement.Application.Interfaces.Services;
+using ClinicManagement.Domain.Entities;
 using FluentValidation;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ClinicManagement.Application.Services
 {
-    public class AuthService:IAuthService
+    public class AuthService : IAuthService
     {
         private readonly IDoctorRepository _doctorRepository;
         private readonly IRefreshTokenRepository _refreshTokenRepository;
@@ -28,9 +31,54 @@ namespace ClinicManagement.Application.Services
             _loginValidator = loginValidator;
 
         }
+        public async Task<Result<LoginServiceResult>> LoginAsync(DoctorLoginRequest request)
+        {
 
+            var validationResult = await _loginValidator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                var error = FormatValidationErrors(validationResult.Errors);
+                return error;
+            }
+            var doctor = await _doctorRepository.GetByMedicalIdAsync(request.MedicalId);
+            if (doctor is null)
+            {
+                return Error.Unauthorized("Auth.InvalidCredentials",
+                "Invalid medical ID or password.");
+            }
+            if (!_passwordHasher.IsMatch(request.Password, doctor.PasswordHash))
+            {
+                return Error.Unauthorized("Auth.InvalidCredentials",
+               "Invalid medical ID or password.");
 
-       
-            
+            }
+            var accessToken = _jwtTokenGenerator.GenerateAccessToken(doctor);
+            var refreshToken = _jwtTokenGenerator.GenerateRefreshToken();
+            var newRefreshToken = RefreshToken.Create(doctor.MedicalId, refreshToken,
+    TimeSpan.FromDays(7));
+       await _refreshTokenRepository.AddAsync(newRefreshToken);
+            var result = new LoginServiceResult(accessToken,refreshToken,
+                newRefreshToken.ExpiresAt,doctor.MedicalId);
+            return result;
+        }
+        private Error FormatValidationErrors(List<ValidationFailure> failures)
+        {
+            string aggregatedErrors = string.Join(" | ", failures.Select(f => $"{f.PropertyName}: {f.ErrorMessage}"));
+            // Using ErrorType.Validation and a generic code for all validation failures
+            return Error.Validation(
+                "Model.Validation", // A generic code for validation issues
+                $"Input validation failed: {aggregatedErrors}"
+            );
+        }
+        public async Task<Result<AuthResponse>> RefreshTokenAsync(string refreshToken)
+        {
+
+        }
+
+        public async Task RevokeRefreshTokenAsync(string refreshToken)
+        {
+            var validationResult = await _loginValidator.ValidateAsync(request);
+        }
+
     }
 }
