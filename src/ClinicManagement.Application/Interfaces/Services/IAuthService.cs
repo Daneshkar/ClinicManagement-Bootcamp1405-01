802500e0b2ab67d5d1447e817828e9df4fc0278c
elena <HP@LAPTOP-GORF2L20>
2026-08-13 12:24:22 +0330

    interface

--- src/ClinicManagement.Application/Interfaces/Services/IAuthService.cs
+++ src/ClinicManagement.Application/Interfaces/Services/IAuthService.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ClinicManagement.Application.Services
+namespace ClinicManagement.Application.Interfaces.Services
 {
     public interface IAuthService
     {
